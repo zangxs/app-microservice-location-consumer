a38e71a67d0dc61141607aac391fb9266c4138b5
brayan <brayanstivenp578@gmail.com>
2026-03-28 16:46:12 -0500

    telegram service

--- src/main/java/com/brayanpv/app/service/contracts/ITelegramService.java
+++ src/main/java/com/brayanpv/app/service/contracts/ITelegramService.java
@@ -5,4 +5,6 @@ import reactor.core.publisher.Mono;
 
 public interface ITelegramService {
     Mono<Void> sendPhoto(TelegramMessage message);
+    Mono<Void> sendApprovalButtons(TelegramMessage message);
+    Mono<Void> sendLocation(TelegramMessage message);
 }
--- src/main/java/com/brayanpv/app/service/implementations/ConsumerService.java
+++ src/main/java/com/brayanpv/app/service/implementations/ConsumerService.java
@@ -4,6 +4,7 @@ import com.brayanpv.app.component.mapper.LandscapeEventMapper;
 import com.brayanpv.app.model.message.LandscapeEvent;
 import com.brayanpv.app.model.request.TelegramMessage;
 import com.brayanpv.app.service.contracts.IConsumerService;
+import com.brayanpv.app.service.contracts.ITelegramService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -15,11 +16,17 @@ import org.springframework.stereotype.Service;
 public class ConsumerService implements IConsumerService {
 
     private final LandscapeEventMapper mapper;
+    private final ITelegramService telegramService;
 
     @Override
     @RabbitListener(queues = "${app.rabbitmq.queue}")
     public void consume(LandscapeEvent event) {
         log.info("Event received: {}", event.landscapeId());
         TelegramMessage message = mapper.toTelegramMessage(event);
+
+        telegramService.sendPhoto(message)
+                .then(telegramService.sendLocation(message))
+                .then(telegramService.sendApprovalButtons(message))
+                .subscribe();
     }
 }
