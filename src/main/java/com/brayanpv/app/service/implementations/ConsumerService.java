2b935679751d948b5fc04fd6534d47e82ae3a70f
brayan <brayanstivenp578@gmail.com>
2026-03-28 18:57:40 -0500

    agregado webhook y su logica para actualizar en db

--- src/main/java/com/brayanpv/app/service/implementations/ConsumerService.java
+++ src/main/java/com/brayanpv/app/service/implementations/ConsumerService.java
@@ -4,6 +4,7 @@ import com.brayanpv.app.component.mapper.LandscapeEventMapper;
 import com.brayanpv.app.model.message.LandscapeEvent;
 import com.brayanpv.app.model.request.TelegramMessage;
 import com.brayanpv.app.model.response.ApiResponse;
+import com.brayanpv.app.repositories.contracts.ILandscapeRepository;
 import com.brayanpv.app.service.contracts.IConsumerService;
 import com.brayanpv.app.service.contracts.ITelegramService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,9 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 @Service
 @RequiredArgsConstructor
 @Log4j2
@@ -19,6 +23,7 @@ public class ConsumerService implements IConsumerService {
 
     private final LandscapeEventMapper mapper;
     private final ITelegramService telegramService;
+    private final ILandscapeRepository  landscapeRepository;
 
     @Override
     @RabbitListener(queues = "${app.rabbitmq.queue}")
@@ -34,6 +39,21 @@ public class ConsumerService implements IConsumerService {
 
     @Override
     public Mono<ApiResponse> processCallback(String data) {
-        return null;
+
+        String[] parts = data.split(":");
+        String action = parts[0];
+        String landscapeId = parts[1];
+
+        log.info("Processing callback: action={}, landscapeId={}", action, landscapeId);
+
+        String status = action.equals("APPROVE") ? "APPROVED" : "REJECTED";
+
+        return landscapeRepository.updateStatus(status, landscapeId)
+                .thenReturn(ApiResponse.builder()
+                        .dateTime(LocalDateTime.now(ZoneOffset.UTC))
+                        .code(200)
+                        .data(landscapeId)
+                        .build());
+
     }
 }
